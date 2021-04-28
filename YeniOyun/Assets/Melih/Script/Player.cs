1d583a03acbcc85239c19947f51e1a877a31cfbb
maceder <topal.melihcan99@gmail.com>
2021-04-28 18:38:24 +0300

    Melih

---
+++ YeniOyun/Assets/Melih/Script/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletObject = Instantiate(bulletPrefab);
+            bulletObject.transform.position = transform.position + transform.forward;
+        }
+    }
+}
