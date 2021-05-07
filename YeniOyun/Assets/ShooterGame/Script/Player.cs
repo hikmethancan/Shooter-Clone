98c2079323518d435db14dc9631e649b725995e3
suheyl <suheyloztrk@icloud.com>
2021-05-07 19:20:45 +0600

    Dosya düzeltmeleri
    -Joystick Pack gereksizleri silindi
    -Joystick kodu player scriptine geçirildi

--- YeniOyun/Assets/ShooterGame/Script/Player.cs
+++ YeniOyun/Assets/ShooterGame/Script/Player.cs
@@ -5,30 +5,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public FixedJoystick variableJoystick;
+    public float rotationSpeed;
     public Transform handPos;
     public List<GameObject> ammoList = new List<GameObject>();
     public GameObject shurikenPrefab;
     public LineRenderer lineRenderer;
     private int index;
-    public int ammo,shrukenSpeed;
+    public int ammo, shrukenSpeed;
     public bool fire;
+
     private void Start()
     {
         fire = false;
     }
+
     private void Update()
     {
+        Joystick();
         Vector3 from = handPos.position;
         Vector3 to = transform.forward;
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, from);
         index = 0;
-        if (!fire) DrawLine(from,to);
-        
+        if (!fire) DrawLine(from, to);
+
 
 
     }
-    void DrawLine(Vector3 from,Vector3 to)
+
+    void DrawLine(Vector3 from, Vector3 to)
     {
         if (index > 5) return;
         if (Physics.Raycast(from, to, out RaycastHit hit))
@@ -39,8 +45,8 @@ public class Player : MonoBehaviour
                 lineRenderer.SetPosition(index, hit.point);
                 to = Vector3.Reflect(to, hit.normal);
                 from = hit.point;
-                if(hit.transform.gameObject.CompareTag("Wall"))
-                    DrawLine(from,to);
+                if (hit.transform.gameObject.CompareTag("Wall"))
+                    DrawLine(from, to);
             }
         }
         else
@@ -51,25 +57,28 @@ public class Player : MonoBehaviour
             lineRenderer.SetPosition(index, to);
         }
     }
+
     public void FireButton()
     {
-        
-            
-            ammoList.Last().SetActive(false);
-            ammoList.Remove(ammoList.Last());
-            GameObject shuriken = Instantiate(shurikenPrefab, handPos.position, transform.rotation);
-            shuriken.GetComponent<Rigidbody>().velocity = shuriken.transform.forward * shrukenSpeed;
-            ammo--;
-            
-            StartCoroutine(Timer(shuriken));
-        
+
+
+        ammoList.Last().SetActive(false);
+        ammoList.Remove(ammoList.Last());
+        GameObject shuriken = Instantiate(shurikenPrefab, handPos.position, transform.rotation);
+        shuriken.GetComponent<Rigidbody>().velocity = shuriken.transform.forward * shrukenSpeed;
+        ammo--;
+
+        StartCoroutine(Timer(shuriken));
+
     }
+
     IEnumerator Timer(GameObject shuriken)
     {
         yield return new WaitForSeconds(3f);
         fire = false;
         Destroy(shuriken);
     }
+
     public void Throw()
     {
         if (!fire && ammo > 0)
@@ -78,10 +87,22 @@ public class Player : MonoBehaviour
             GetComponent<Animator>().SetTrigger("Throw");
         }
     }
+
     public void Win()
     {
         fire = true;
         GetComponent<Animator>().SetTrigger("Win");
+
+
     }
 
+    private void Joystick()
+    {
+        float angle = Mathf.Atan2(variableJoystick.Horizontal, variableJoystick.Vertical) * Mathf.Rad2Deg;
+        if(angle!=0){
+            angle = Mathf.MoveTowardsAngle(transform.rotation.eulerAngles.y, angle, rotationSpeed);
+            transform.rotation = Quaternion.Euler(0,angle,0);
+
+        }
+    }
 }
